890e2ace480d56aaa88b71cd0266e06329d52f06
Cakecrook <willtaylor116@gmail.com>
2021-06-08 17:19:01 -0400

    Made some progress on home page and animal cards

--- frontend/fmp-frontend/src/index.js
+++ frontend/fmp-frontend/src/index.js
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import LostAnimals from "./Components/LostAnimals"
 
 ReactDOM.render(
   <div className="container-fluid">
     <div className="row">
       <div className="col">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-3">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light border border-secondary rounded mt-3">
           <a className="navbar-brand">
             <h1>Find My Paws</h1>
           </a>
         </nav>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-7">
+        <LostAnimals />
       </div>
     </div>
   </div>,
