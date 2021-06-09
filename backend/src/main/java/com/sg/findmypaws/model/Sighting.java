e881b279829dae0f3ef5ba14896eec7147bfbab0
Tyler Lozano <tyler.m.lozano@gmail.com>
2021-06-09 11:35:03 -0400

    sightings

--- backend/src/main/java/com/sg/findmypaws/model/Sighting.java
+++ backend/src/main/java/com/sg/findmypaws/model/Sighting.java
@@ -4,12 +4,41 @@ import java.util.Date;
 
 public class Sighting {
     public int id;
-    public Location location;
-    public Animal animal;
+//    public Location location;
+//    public Animal animal;
+    public int locationId;
+    public int animalId;
     public Date date;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public int getLocationId() {
+        return locationId;
+    }
 
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
 
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(int animalId) {
+        this.animalId = animalId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
---
+++ backend/src/main/java/com/sg/findmypaws/service/SightingsService.java
@@ -0,0 +1,25 @@
+package com.sg.findmypaws.service;
+
+import com.sg.findmypaws.dao.SightingDaoDB;
+import com.sg.findmypaws.model.Animal;
+import com.sg.findmypaws.model.Location;
+import com.sg.findmypaws.model.Sighting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SightingsService {
+
+//    @Override
+//    public List<Sighting> getAllFilteredSightingsWithFilter(Location loc, int radius, int timeDeltaInDays, Animal animal) {
+//        List<Location> locs = locationDaoDB.getAllLocationsWithFilter(loc, radius);
+//        List<Sighting> sightings = new ArrayList<>();
+//        for (Location l : locs) {
+//            sightings.addAll(getAllSightingsByLocId(l.getId()));
+//        }
+//        sightings.removeIf(s -> s.animalId)
+//
+//    }
+
+
+}
