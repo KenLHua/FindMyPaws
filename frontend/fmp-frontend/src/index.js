bf61706d2f0dbc011a5274e9c0b4515196008f86
Cakecrook <willtaylor116@gmail.com>
2021-06-10 02:53:57 -0400

    added a basic theme change button, still needs some work

--- frontend/fmp-frontend/src/index.js
+++ frontend/fmp-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css' // It IS in use, just doesn't highlight
+// import bootstrap from 'bootstrap/dist/css/bootstrap.min.css' // It IS in use, just doesn't highlight
 import App from "./App"
 
 
